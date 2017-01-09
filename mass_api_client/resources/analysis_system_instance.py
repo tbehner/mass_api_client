078d5ec68796dab53884e1adf6f7b67edbd75979
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-01-09 23:32:16 +0100

    Retrieve scheduled analyses of a analysis system instance

--- mass_api_client/resources/analysis_system_instance.py
+++ mass_api_client/resources/analysis_system_instance.py
@@ -10,6 +10,10 @@ class AnalysisSystemInstance(BaseResource):
     def schedule_analysis(self, sample):
         return ScheduledAnalysis.create(self, sample)
 
+    def get_scheduled_analyses(self):
+        url = '{}scheduled_analyses/'.format(self.url)
+        return ScheduledAnalysis._get_list_from_url(url, append_base_url=False)
+
     def __repr__(self):
         return '[AnalysisSystemInstance] {}'.format(self.uuid)
 
