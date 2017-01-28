d51d8c0b0eacbcbe58e2158c1549c8e6fe8fcf65
Christopher Schmidt <Silberschleier@users.noreply.github.com>
2017-01-28 13:38:17 +0100

    Changed name

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@
 
 from distutils.core import setup
 
-setup(name='MASS API Client',
+setup(name='mass_api_client',
       version=0.1,
       install_required=['requests==2.13.0', 'marshmallow==2.12.1'])
