30e407eda33ef55bdfb0cc96ccc6ee889e7604bd
Christopher Schmidt <Silberschleier@users.noreply.github.com>
2017-01-30 15:34:29 +0100

    Updated marshmallow

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from distutils.core import setup
 
 setup(name='mass_api_client',
       version=0.1,
-      install_required=['requests==2.13.0', 'marshmallow==2.12.1'])
+      install_required=['requests==2.13.0', 'marshmallow==2.12.2'])
