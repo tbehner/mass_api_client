f91bbe830a4863f0665216dea8bce320cc58f601
requires.io <support@requires.io>
2017-02-18 19:00:57 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 
 setup(name='mass_api_client',
       version=0.1,
-      install_requires=['requests==2.13.0', 'marshmallow==2.12.2'],
+      install_requires=['requests==2.13.0', 'marshmallow==2.13.0'],
       packages=['mass_api_client', ],
       )
