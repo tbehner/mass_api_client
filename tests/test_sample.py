0b977ece0ba78771be9f1529c9c6c9cecf42286b
Timm Behner <behner@fkie.fraunhofer.de>
2017-03-01 16:15:41 +0100

    Test FileSample temporary_file

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -2,6 +2,8 @@ import json
 
 from mass_api_client.resources import DomainSample, IPSample, URISample, FileSample, ExecutableBinarySample
 from tests.serialization_test_case import SerializationTestCase
+from tests.httmock_test_case import HTTMockTestCase
+from httmock import urlmatch, HTTMock
 
 
 class DomainSampleTestCase(SerializationTestCase):
@@ -28,12 +30,28 @@ class URISampleTestCase(SerializationTestCase):
         self.assertEqualAfterSerialization(URISample, data)
 
 
-class FileSampleTestCase(SerializationTestCase):
+class FileSampleTestCase(SerializationTestCase, HTTMockTestCase):
     def test_is_data_correct_after_serialization(self):
         with open('tests/data/file_sample.json') as data_file:
             data = json.load(data_file)
 
         self.assertEqualAfterSerialization(FileSample, data)
+
+    def test_temporary_file(self):
+        @urlmatch()
+        def mass_mock(url, req):
+            return b'Content'
+
+        with open('tests/data/file_sample.json') as data_file:
+            data = json.load(data_file)
+
+        file_sample = FileSample._create_instance_from_data(data)
+        with HTTMock(mass_mock):
+            with file_sample.temporary_file() as f:
+                self.assertEqual(f.name[:4], '/tmp')
+                f.seek(0)
+                self.assertEqual(f.read(), b'Content')
+
 
 
 class ExecutableBinarySampleTestCase(SerializationTestCase):
