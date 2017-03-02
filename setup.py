8b86384a3784f1a39161b3d86ffd65613b1d9951
Timm Behner <behner@fkie.fraunhofer.de>
2017-03-02 14:06:04 +0100

    Added packages

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ from distutils.core import setup
 setup(name='mass_api_client',
       version=0.1,
       install_requires=['requests==2.13.0', 'marshmallow==2.13.0'],
-      packages=['mass_api_client', ],
-      )
+      packages=['mass_api_client',
+                'mass_api_client.resources',
+                'mass_api_client.schemas'
+                ]
+     )
